a57138d30c8a7b0b7ed0d859f35f347918a16e82
Antonín Bujnovský <120708132+MrBujnowski@users.noreply.github.com>
2025-01-21 22:11:19 +0000

    Add files via upload

---
+++ migrations/versions/20250121_v3.py
@@ -0,0 +1,24 @@
+"""v3
+
+Revision ID: 20250121_v3
+Revises: 20250121_v2
+Create Date: 2025-01-21 20:49:38.748083+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '20250121_v3'
+down_revision = '20250121_v2'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
